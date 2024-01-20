8a2ae2299505e127a1f48c710f6183a7dd658dfa
AFabulousWhale <maraltom@gmail.com>
2024-01-20 17:22:46 +0000

    added basic particle effects for the thruster

--- Assets/Scripts/Physics/Thruster.cs
+++ Assets/Scripts/Physics/Thruster.cs
@@ -6,10 +6,11 @@ public class Thruster : MonoBehaviour
 {
     Rigidbody rb;
     public float thrustAmount = 20f;
-
+    GameObject fireParticle;
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        fireParticle = transform.GetChild(0).gameObject;
     }
 
     void FixedUpdate()
@@ -17,7 +18,12 @@ public class Thruster : MonoBehaviour
         if (Input.GetButton("Jump"))
         {
             //Apply a force to this Rigidbody in direction of this GameObjects up axis
+            fireParticle.SetActive(true);
             rb.AddForce(transform.up * thrustAmount);
+        }
+        else
+        {
+            fireParticle.SetActive(false);
         }
     }
 }
